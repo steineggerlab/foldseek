920529d618f4399d2e1b9e7c21fd6b310671dee2
schnamo <ch.tumescheit@gmail.com>
2021-10-05 19:09:37 +0900

    length bug fixing and tidy up

--- src/strucclustutils/pareunaligner.cpp
+++ src/strucclustutils/pareunaligner.cpp
@@ -16,10 +16,10 @@
 #include <omp.h>
 #endif
 
-#include <math.h>
+#include <cmath>
 
 // static two dimensional array instead of vectors
-vector<int> findNearestNeighbour(char * nn, Coordinates & ca, int length, char *sseq, Sequence & seqAll){
+void findNearestNeighbour(char * nn, Coordinates & ca, int length, unsigned char * seqInt){
     // (char*)mem_align(ALIGN_INT, par.maxSeqLen * sizeof(char) * 4 );
 
     // calculate pairwise matrix (euclidean distance)
@@ -29,6 +29,7 @@ vector<int> findNearestNeighbour(char * nn, Coordinates & ca, int length, char *
     float seqDist;
 
     for(int i = 0; i < length; i++){
+
         std::vector<std::pair<float, int>> seqDistList;
         for(int j = 0; j < length; j++){
             if(i != j) {
@@ -36,49 +37,45 @@ vector<int> findNearestNeighbour(char * nn, Coordinates & ca, int length, char *
                 seqDist = sqrt(
                         ((ca.x[i] - ca.x[j]) * (ca.x[i] - ca.x[j])) + ((ca.y[i] - ca.y[j]) * (ca.y[i] - ca.y[j])) +
                         ((ca.z[i] - ca.z[j]) * (ca.z[i] - ca.z[j])));
-                seqDistList.push_back(make_pair(seqDist, j));
+                seqDistList.emplace_back(make_pair(seqDist, j));
             }
         }
         // find the four nearest neighbours for each amino acid
         std::sort(seqDistList.begin(), seqDistList.end());
         int pos[4];
+
         for(int m  = 0; m < 4; m++){
             pos[m]  = seqDistList[m].second;
         }
         std::sort(begin(pos), end(pos));
         for(int n = 0; n < 4; n++){
-//            cout << sseq[pos[n]] << endl;
-//            nn[i*4 + n]  = sseq[pos[n]];
-            nnInt.push_back(seqAll.numSequence[pos[n]]);
+            int neighbour = static_cast<int>(seqInt[pos[n]]);
+            nn[i*4 + n] = neighbour;
         }
-//        for(int k = 0;   k < seqDistList.size(); k++){
-//            cout << seqDistList[k].first << " : " << seqDistList[k].second << endl;
-//        }
         seqDistList.clear();
-
-
-//        for(int l = 0; l < 4; l++){ cout << pos[l] <<  endl;}
-//        cout << "--------" << endl;
     }
-
-//    for(int a = 0; a < 4*nnInt.size(); a++){
-//        cout << nnInt[a] << endl;
-//    }
-
-    return nnInt;
 }
 
-int needlemanWunschScore(int subQNNi[4], int subTNNi[4], SubstitutionMatrix *subMat, int nwGapPenalty){
-//    LocalParameters& par = LocalParameters::getLocalInstance();
-//    int nwGapPenalty = par.gapNW ;
+short needlemanWunschScore(int subQNNi[4], int subTNNi[4], SubstitutionMatrix *subMat, int nwGapPenalty){
 
-    int scoringMatrix[5][5] = {0};
+    int scoringMatrix[5][5] = {{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0}};
 
     for(int i = 1; i < 5; i++){
         for(int j = 1; j < 5; j++){
+
+            int score1 = subQNNi[i - 1];
+            int score2 = subTNNi[j - 1];
+            short scoreTest;
+
+//            if(score1 < 0 or score1 > 15){cout << " NO " << score1;}
+//            if(score2 < 0 or score2 > 15){cout << " NO " << score2;}
+
+            scoreTest = subMat->subMatrix[score1][score2];
+
             // calculate scores - bonus for base pairing and penalty for not
-            scoringMatrix[i][j] = std::max(scoringMatrix[i-1][j-1] + subMat->subMatrix[subQNNi[i - 1]][subTNNi[j - 1]], scoringMatrix[i-1][j] - nwGapPenalty);
+            scoringMatrix[i][j] = std::max(scoringMatrix[i-1][j-1] + scoreTest, scoringMatrix[i-1][j] - nwGapPenalty);
             scoringMatrix[i][j] = std::max(scoringMatrix[i][j-1] - nwGapPenalty, scoringMatrix[i][j]);
+
         }
     }
 
@@ -90,50 +87,55 @@ int needlemanWunschScore(int subQNNi[4], int subTNNi[4], SubstitutionMatrix *sub
 //        }
 //        cout << endl;
 //    }
-    int score = scoringMatrix[4][4] / 4;
+    short score = scoringMatrix[4][4]; /// 4;
 
     return score;
 }
 
-Matcher::result_t alignByNN(char * querynn, vector<int> nnIntQuery, char *querySSeq, int queryLen, char * targetnn, vector<int> nnIntTarget, char *targetSSeq, int targetLen, SubstitutionMatrix *subMat, Sequence & qSeq, Sequence & tSeq, int gapOpen, int gapExtern, int gapNW, float nnWeight, EvalueComputation & evaluer){
+Matcher::result_t alignByNN(char * querynn, unsigned char *querySeqInt, int queryLen, char * targetnn, unsigned char *targetSeqInt, int targetLen, SubstitutionMatrix *subMat, int gapOpen, int gapExtern, int gapNW, float nnWeight){
 
     Matcher::result_t result;
     // gotoh sw itself
 
         struct scores{ short H, E, F; };
         uint16_t max_score = 0;
-        char query[4], target[4];
+
         scores *workspace = new scores[queryLen * 2 + 2];
         scores *curr_sHEF_vec = &workspace[0];
         scores *prev_sHEF_vec = &workspace[queryLen + 1];
-        char subQNN[4], subTNN[4];
+
         int subTNNi[4], subQNNi[4];
         // top row need to be set to a 0 score
         memset(prev_sHEF_vec, 0, sizeof(scores) * (queryLen + 1));
         for (int i = 0; i < targetLen; i++) {
 
-            for(int a=0; a <4; a++){ subTNNi[a]=nnIntTarget[4*i + a];
-//                cout << targetSSeq[4*i + a];
+            for(int a = 0; a < 4; a++){
+                int neighbour = static_cast<int>(targetnn[4*i + a]);
+                subTNNi[a] =  neighbour;
             }
-//            cout << endl;
+
             // left outer column need to be set to a 0 score
             prev_sHEF_vec[0].H = 0; prev_sHEF_vec[0].E = 0; prev_sHEF_vec[0].F = 0;
             curr_sHEF_vec[0].H = 0; curr_sHEF_vec[0].E = 0; curr_sHEF_vec[0].E = 0;
             for (int j = 1; j <= queryLen; j++) {
-                for(int a=0; a <4; a++){subQNNi[a]=nnIntQuery[4*(j-1) + a];
-//                    cout << querySSeq[4*(j-1) + a];
+                for(int a = 0; a < 4; a++){
+                    int neighbour = static_cast<int>(querynn[4*(j-1) + a]);
+                    subQNNi[a] = neighbour;
                 }
-//                cout << endl;
-//                for(int a=0; a <4; a++){
-//                    cout << targetSSeq[4*i + a];
-//                }
-//                cout << endl;
+
                 curr_sHEF_vec[j].E = std::max(curr_sHEF_vec[j - 1].H - gapOpen, curr_sHEF_vec[j - 1].E - gapExtern); // j-1
                 curr_sHEF_vec[j].F = std::max(prev_sHEF_vec[j].H - gapOpen, prev_sHEF_vec[j].F - gapExtern); // i-1
-                int bla = needlemanWunschScore(subTNNi, subQNNi, subMat, gapNW);
-//                cout << nnWeight << endl;
+                short bla = needlemanWunschScore(subTNNi, subQNNi, subMat, gapNW);
                 bla = nnWeight * bla;
-                const short tempH = prev_sHEF_vec[j - 1].H + subMat->subMatrix[tSeq.numSequence[i]][qSeq.numSequence[j - 1]] + bla; // i - 1, j - 1
+                int subOne = static_cast<int>(targetSeqInt[i]);
+                int subTwo = static_cast<int>(querySeqInt[j - 1]);
+//                if(subOne < 0 or subOne > 15){cout << " NO " << subOne;}
+//                if(subTwo < 0 or subTwo > 15){cout << " NO " << subTwo;}
+                int test_shit = 0;
+                if(subOne == subTwo){test_shit = 2;}else{test_shit = -1;}
+                const short tempH = prev_sHEF_vec[j - 1].H + subMat->subMatrix[subOne][subTwo] + bla; // i - 1, j - 1
+//                const short tempH = prev_sHEF_vec[j - 1].H + subMat->subMatrix[subOne][subTwo]  + bla; // i - 1, j - 1
+
                 curr_sHEF_vec[j].H = std::max(tempH, curr_sHEF_vec[j].E);
                 curr_sHEF_vec[j].H = std::max(curr_sHEF_vec[j].H, curr_sHEF_vec[j].F);
                 curr_sHEF_vec[j].H = std::max(curr_sHEF_vec[j].H, static_cast<short>(0));
@@ -143,7 +145,7 @@ Matcher::result_t alignByNN(char * querynn, vector<int> nnIntQuery, char *queryS
         }
         delete [] workspace;
 
-//        cout << "max score: " << max_score << endl;
+//        cout << max_score << " ";
 //    result.backtrace = optAlnResult.cigar_string;
     result.score = max_score;
 //    result.qStartPos = optAlnResult.query_start;
@@ -154,8 +156,7 @@ Matcher::result_t alignByNN(char * querynn, vector<int> nnIntQuery, char *queryS
 //    result.qcov = SmithWaterman::computeCov(result.qStartPos, result.qEndPos, qSeq.L);
     //result.tCov = SmithWaterman::computeCov(result.dbStartPos1, result.dbEndPos1, targetSeqObj->L);
 //    result.dbcov = SmithWaterman::computeCov(result.dbStartPos, result.dbEndPos, tSeq.L);
-    result.eval = evaluer.computeEvalue(result.score, queryLen);
-
+//    result.eval = evaluer.computeEvalue(result.score, queryLen);
 
     return result;
 }
@@ -230,7 +231,7 @@ int pareunaligner(int argc, const char **argv, const Command& command) {
         char buffer[1024+32768];
         std::string resultBuffer;
         //int gapOpen = 15; int gapExtend = 3; // 3di gap optimization
-        EvalueComputation evaluer(tdbr->getAminoAcidDBSize(), &subMat, par.gapOpen.aminoacids, par.gapExtend.aminoacids);
+//        EvalueComputation evaluer(tdbr->getAminoAcidDBSize(), &subMat, par.gapOpen.aminoacids, par.gapExtend.aminoacids);
         // write output file
 
 #pragma omp for schedule(dynamic, 1)
@@ -241,20 +242,20 @@ int pareunaligner(int argc, const char **argv, const Command& command) {
             if(*data != '\0') {
                 unsigned int queryId = qdbr.getId(queryKey);
                 char *querySeq = qdbr.getData(queryId, thread_idx);
-                unsigned int querySeqLen = qdbr.getSeqLen(id);
+                unsigned int querySeqLen = qdbr.getSeqLen(queryId);
                 qSeq.mapSequence(id, queryKey, querySeq, querySeqLen);
 
-                int queryLen = static_cast<int>(qdbr.getSeqLen(queryId));
                 float *qdata = (float *) qcadbr.getData(queryId, thread_idx);
                 Coordinates queryCaCords;
-                memcpy(query_x, qdata, sizeof(float) * queryLen);
-                memcpy(query_y, &qdata[queryLen], sizeof(float) * queryLen);
-                memcpy(query_z, &qdata[queryLen+queryLen], sizeof(float) * queryLen);
+                memcpy(query_x, qdata, sizeof(float) * qSeq.L);
+                memcpy(query_y, &qdata[qSeq.L], sizeof(float) * qSeq.L);
+                memcpy(query_z, &qdata[qSeq.L+qSeq.L], sizeof(float) * qSeq.L);
 
                 queryCaCords.x = query_x;
                 queryCaCords.y = query_y;
                 queryCaCords.z = query_z;
-                vector<int> nnIntQuery = findNearestNeighbour(querynn, queryCaCords, querySeqLen, querySeq, qSeq);
+
+                findNearestNeighbour(querynn, queryCaCords, querySeqLen, qSeq.numSequence);
 
                 int passedNum = 0;
                 int rejected = 0;
@@ -265,59 +266,32 @@ int pareunaligner(int argc, const char **argv, const Command& command) {
                     const unsigned int dbKey = (unsigned int) strtoul(dbKeyBuffer, NULL, 10);
                     unsigned int targetId = tdbr->getId(dbKey);
                     const bool isIdentity = (queryId == targetId && (par.includeIdentity || sameDB))? true : false;
-                    if(isIdentity == true){
-                        std::string backtrace = "";
 
-                        int anat= (queryLen%4) ? (queryLen/4)*4+4 : queryLen;
-                        for(int i=queryLen;i<anat;i++){
-                            query_x[i]=0.0f;
-                            query_x[i]=0.0f;
-                            query_x[i]=0.0f;
-                        }
-
-                        Matcher::result_t result(dbKey, 0 , 1.0, 1.0, 1.0, 1.0, std::max(queryLen,queryLen), 0, queryLen-1, queryLen, 0, queryLen-1, queryLen, backtrace);
-                        size_t len = Matcher::resultToBuffer(buffer, result, true, false);
-                        resultBuffer.append(buffer, len);
-                        continue;
-                    }
                     char * targetSeq = tdbr->getData(targetId, thread_idx);
-                    unsigned int targetSeqLen = tdbr->getSeqLen(targetId);
 
                     int targetLen = static_cast<int>(tdbr->getSeqLen(targetId));
                     float * tdata = (float*) tcadbr->getData(targetId, thread_idx);
-                    tSeq.mapSequence(targetId, dbKey, targetSeq, targetSeqLen);
+                    tSeq.mapSequence(targetId, dbKey, targetSeq,tdbr->getSeqLen(targetId));
 
-                    if(Util::canBeCovered(par.covThr, par.covMode, queryLen, targetLen)==false){
+                    if(Util::canBeCovered(par.covThr, par.covMode, qSeq.L, targetLen)==false){
                         continue;
                     }
                     Coordinates targetCaCords;
-                    memcpy(target_x, tdata, sizeof(float) * targetLen);
-                    memcpy(target_y, &tdata[targetLen], sizeof(float) * targetLen);
-                    memcpy(target_z, &tdata[targetLen+targetLen], sizeof(float) * targetLen);
+                    memcpy(target_x, tdata, sizeof(float) * tSeq.L);
+                    memcpy(target_y, &tdata[tSeq.L], sizeof(float) * tSeq.L);
+                    memcpy(target_z, &tdata[tSeq.L+tSeq.L], sizeof(float) * tSeq.L);
 
                     targetCaCords.x = target_x;
                     targetCaCords.y = target_y;
                     targetCaCords.z = target_z;
-                    vector<int> nnIntTarget = findNearestNeighbour(targetnn, targetCaCords, targetSeqLen, targetSeq, tSeq);
-                    Matcher::result_t res = alignByNN(querynn, nnIntQuery, querySeq, queryLen, targetnn, nnIntTarget, targetSeq, targetSeqLen, &subMat, qSeq, tSeq ,par.gapOpen.aminoacids, par.gapExtend.aminoacids, par.gapNW, par.nnWeight, evaluer);
+
+                    findNearestNeighbour(targetnn, targetCaCords, tSeq.L, tSeq.numSequence);
+                    Matcher::result_t res = alignByNN(querynn, qSeq.numSequence, qSeq.L, targetnn, tSeq.numSequence, tSeq.L, &subMat, par.gapOpen.aminoacids, par.gapExtend.aminoacids, par.gapNW, par.nnWeight);
                     unsigned int targetKey = tdbr->getDbKey(targetId);
                     res.dbKey = targetKey;
                     //Matcher::result_t res = paruenAlign.align(qSeq, tSeq, &subMat, evaluer);
                     string cigar =  paruenAlign.backtrace2cigar(res.backtrace);
 
-                    if (isIdentity) {
-                        // set coverage and seqid of identity
-                        res.qcov = 1.0f;
-                        res.dbcov = 1.0f;
-                        res.seqId = 1.0f;
-                    }
-
-                    //Add TM align score
-                    Coordinates xtm(queryLen); Coordinates ytm(targetLen);
-                    Coordinates r1(queryLen); Coordinates r2(targetLen);
-
-                    // Matching residue index collection
-                    paruenAlign.AlignedResidueIndex(res, ires);
 
                     // float t[3], u[3][3];
                     //double TMalnScore = get_score4pareun(r1, r2,  xtm, ytm, queryCaCords, targetCaCords, ires,
