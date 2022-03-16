d09a3c4b0db2a130607c968c39d854e73df9de35
Cameron Gilchrist <clmgilchrist@gmail.com>
2022-03-16 13:48:15 +0900

    Create easymsa.sh

---
+++ data/easymsa.sh
@@ -0,0 +1,93 @@
+#!/bin/sh -e
+fail() {
+    echo "Error: $1"
+    exit 1
+}
+
+notExists() {
+	[ ! -f "$1" ]
+}
+
+if notExists "${TMP_PATH}/query.dbtype"; then
+    # shellcheck disable=SC2086
+    "$MMSEQS" createdb "$@" "${TMP_PATH}/query" ${CREATEDB_PAR} \
+        || fail "query createdb died"
+fi
+
+if notExists "${TARGET}.dbtype"; then
+    if notExists "${TMP_PATH}/target"; then
+        # shellcheck disable=SC2086
+        "$MMSEQS" createdb "${TARGET}" "${TMP_PATH}/target" ${CREATEDB_PAR} \
+            || fail "target createdb died"
+    fi
+    TARGET="${TMP_PATH}/target"
+fi
+
+
+INTERMEDIATE="${TMP_PATH}/result"
+if notExists "${INTERMEDIATE}.dbtype"; then
+    # shellcheck disable=SC2086
+    "$MMSEQS" search "${TMP_PATH}/query" "${TARGET}" "${INTERMEDIATE}" "${TMP_PATH}/search_tmp" ${SEARCH_PAR} \
+        || fail "Search died"
+fi
+
+if [ -n "${GREEDY_BEST_HITS}" ]; then
+    if notExists "${TMP_PATH}/result_best.dbtype"; then
+        # shellcheck disable=SC2086
+        $RUNNER "$MMSEQS" summarizeresult "${TMP_PATH}/result" "${TMP_PATH}/result_best" ${SUMMARIZE_PAR} \
+            || fail "Search died"
+    fi
+    INTERMEDIATE="${TMP_PATH}/result_best"
+fi
+
+if notExists "${TMP_PATH}/alis.dbtype"; then
+    # shellcheck disable=SC2086
+	# querydb targetdb alndb alnfile
+    "$MMSEQS" convertalis "${TMP_PATH}/query" "${TARGET}${INDEXEXT}" "${INTERMEDIATE}" "${RESULTS}" ${CONVERT_PAR} \
+        || fail "Convert Alignments died"
+fi
+
+if notExists "${TMP_PATH}/${TREE}"; then
+    # shellcheck disable=SC2086
+	# Query DB, Target DB, Alignment DB, Tree file
+	"$MMSEQS" generatetree "${TMP_PATH}/query" "${TARGET}${INDEXEXT}" "${RESULTS}" "${TREE}" \
+		|| fail "Generate Tree died"
+fi
+
+if notExists "${TMP_PATH}/${MSA}"; then
+    # shellcheck disable=SC2086
+	# Query DB, Target DB, Results DB, Tree, MSA
+	"$MMSEQS" traversetree "${TMP_PATH}/query" "${TARGET}${INDEXEXT}" "${RESULTS}" "${TREE}" "${MSA}" \
+		|| fail "Traverse Tree died"
+fi
+
+if [ -n "${REMOVE_TMP}" ]; then
+    if [ -n "${GREEDY_BEST_HITS}" ]; then
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${TMP_PATH}/result_best" ${VERBOSITY}
+    fi
+    # shellcheck disable=SC2086
+    "$MMSEQS" rmdb "${TMP_PATH}/result" ${VERBOSITY}
+    if [ -z "${LEAVE_INPUT}" ]; then
+        if [ -f "${TMP_PATH}/target" ]; then
+            # shellcheck disable=SC2086
+            "$MMSEQS" rmdb "${TMP_PATH}/target" ${VERBOSITY}
+            # shellcheck disable=SC2086
+            "$MMSEQS" rmdb "${TMP_PATH}/target_h" ${VERBOSITY}
+            # shellcheck disable=SC2086
+            "$MMSEQS" rmdb "${TMP_PATH}/target_ca" ${VERBOSITY}
+            # shellcheck disable=SC2086
+            "$MMSEQS" rmdb "${TMP_PATH}/target_ss" ${VERBOSITY}
+        fi
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${TMP_PATH}/query" ${VERBOSITY}
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${TMP_PATH}/query_h" ${VERBOSITY}
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${TMP_PATH}/query_ca" ${VERBOSITY}
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${TMP_PATH}/query_ss" ${VERBOSITY}
+    fi
+    rm -rf "${TMP_PATH}/search_tmp"
+    rm -f "${TMP_PATH}/easymsa.sh"
+fi
