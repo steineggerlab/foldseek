cb8d1b87f0a56fc1c52a53494c6b3079d1330fd2
schnamo <ch.tumescheit@gmail.com>
2022-03-18 17:40:16 +0900

    adjust modules input

--- src/strucclustutils/generatetree.cpp
+++ src/strucclustutils/generatetree.cpp
@@ -22,6 +22,28 @@ int generatetree(int argc, const char **argv, const Command& command) {
     LocalParameters &par = LocalParameters::getLocalInstance();
     par.parseParameters(argc, argv, command, true, 0, MMseqsParameter::COMMAND_ALIGN);
 
+    std::vector<Matcher::result_t> results;
+    DBReader<unsigned int> reader(par.db1.c_str(), par.db1Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
+    reader.open(DBReader<unsigned int>::LINEAR_ACCCESS);
+
+    size_t dbFrom = 0;
+    size_t dbSize = reader.getSize();
+    unsigned int thread_idx = 0;
+
+    for (size_t i = dbFrom; i < dbFrom + dbSize; ++i) {
+        char *data = reader.getData(i, thread_idx);
+
+        bool readFirst = false;
+//        writer.writeStart(thread_idx);
+        while (*data != '\0') {
+            Matcher::result_t domain = Matcher::parseAlignmentRecord(data, true);
+            data = Util::skipLine(data);
+            std::cout << domain.dbKey << ": " << domain.score << std::endl;
+            }
+        }
+//    char *data = alndbr.getData(i, thread_idx);
+//    Matcher::readAlignmentResults(results, data, false);
+
 //    DBReader<unsigned int> alnDbr(par.db3.c_str(), par.db3Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
 //    alnDbr.open(DBReader<unsigned int>::LINEAR_ACCCESS);
 
