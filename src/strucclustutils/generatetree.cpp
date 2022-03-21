502cd2d4a1689cec926eef3508cd13497b037e46
schnamo <ch.tumescheit@gmail.com>
2022-03-21 18:19:37 +0900

    add an insanely high e-value to get all gapped structure scores, make sim matrix in generatetree

--- src/strucclustutils/generatetree.cpp
+++ src/strucclustutils/generatetree.cpp
@@ -9,6 +9,7 @@
 #include "structureto3diseqdist.h"
 #include "StructureSmithWaterman.h"
 #include "StructureUtil.h"
+#include <tuple>
 
 #ifdef OPENMP
 #include <omp.h>
@@ -29,23 +30,33 @@ int generatetree(int argc, const char **argv, const Command& command) {
     size_t dbFrom = 0;
     size_t dbSize = reader.getSize();
     unsigned int thread_idx = 0;
+    std::vector<Matcher::result_t> result;
+
+    int score_list[dbSize][dbSize];
+    int queryKey  = 0;
+
+    for(int n = 0; n < dbSize; n++){
+        for(int m = 0; m < dbSize; m++){
+            score_list[n][m] = 0;
+        }
+    }
 
     for (size_t i = dbFrom; i < dbFrom + dbSize; ++i) {
         char *data = reader.getData(i, thread_idx);
-
-        bool readFirst = false;
-//        writer.writeStart(thread_idx);
-        while (*data != '\0') {
-            Matcher::result_t domain = Matcher::parseAlignmentRecord(data, true);
-            data = Util::skipLine(data);
-            std::cout << domain.dbKey << ": " << domain.score << std::endl;
-            }
+        Matcher::readAlignmentResults(result, data, 0);
+        queryKey = reader.getDbKey(i);
+        for (int j = 0; j < result.size(); j++) {
+            score_list[queryKey][result[j].dbKey] = result[j].score;
         }
-//    char *data = alndbr.getData(i, thread_idx);
-//    Matcher::readAlignmentResults(results, data, false);
+        result.clear();
+    }
 
-//    DBReader<unsigned int> alnDbr(par.db3.c_str(), par.db3Index.c_str(), par.threads, DBReader<unsigned int>::USE_INDEX|DBReader<unsigned int>::USE_DATA);
-//    alnDbr.open(DBReader<unsigned int>::LINEAR_ACCCESS);
+    for(int n = 0; n < dbSize; n++){
+        for(int m = 0; m < dbSize; m++){
+            std::cout << score_list[n][m] << " ";
+        }
+        std::cout << std::endl;
+    }
 
     std::cout << "Hello, worldyMcWorldface!" << std::endl;
     return EXIT_SUCCESS;
