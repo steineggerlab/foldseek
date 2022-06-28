c435a8ba9df6e9420c286647c95b1c925020a6a6
Martin Steinegger <themartinsteinegger@gmail.com>
2022-06-28 11:58:22 +0900

    Add MMseqs2 RBH to Foldseek

--- src/LocalCommandDeclarations.h
+++ src/LocalCommandDeclarations.h
@@ -13,4 +13,7 @@ extern int structurealign(int argc, const char** argv, const Command &command);
 extern int samplemulambda(int argc, const char** argv, const Command &command);
 extern int structureconvertalis(int argc, const char** argv, const Command &command);
 extern int structureto3didescriptor(int argc, const char** argv, const Command &command);
+extern int structurerbh(int argc, const char** argv, const Command &command);
+extern int structureeasyrbh(int argc, const char** argv, const Command &command);
+
 #endif
