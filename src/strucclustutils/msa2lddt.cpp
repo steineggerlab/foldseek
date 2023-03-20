6dd6254d59c05a01c92c5190e01ea3c2ab319076
Cameron Gilchrist <clmgilchrist@gmail.com>
2023-03-20 17:00:50 +0900

    multithreading in msa2lddt

--- src/strucclustutils/msa2lddt.cpp
+++ src/strucclustutils/msa2lddt.cpp
@@ -19,6 +19,15 @@
 #include "LDDT.h"
 #include "Coordinate16.h"
 
+#ifdef OPENMP
+#include <omp.h>
+#endif
+
+#pragma omp declare reduction(vsum : std::vector<float> : \
+    std::transform(omp_in.begin(), omp_in.end(), \
+                   omp_out.begin(), omp_out.begin(), std::plus<float>())) \
+    initializer(omp_priv(omp_orig))
+
 // LDDT scoring
 // - Iterate unique pairs of sequences in MSA
 //   - Generate faux Matcher::result_t
@@ -45,6 +54,7 @@ int msa2lddt(int argc, const char **argv, const Command& command) {
     int maxSeqLength = 0;
     std::vector<std::string> headers;
     std::vector<size_t> indices;
+    std::vector<int> lengths;
     std::vector<std::string> sequences;
     std::vector<std::string> sequences3di;
     while (kseq->ReadEntry()) {
@@ -63,6 +73,7 @@ int msa2lddt(int argc, const char **argv, const Command& command) {
         seqAA.pop_back();
         std::string seq3Di = seqDbr3Di.getData(seqId3Di, 0);
         seq3Di.pop_back();
+        lengths.push_back(seqAA.length());
         for (size_t i = 0; i < entry.sequence.l; i++) {
             if (entry.sequence.s[i] == '-') {
                 seqAA.insert(i, "-");
@@ -80,116 +91,98 @@ int msa2lddt(int argc, const char **argv, const Command& command) {
     
     // Track per-column scores and no. non-gaps to avg
     std::vector<float> perColumnScore(alnLength, 0.0);
-    std::vector<float> perColumnSOP(alnLength, 0.0);
     std::vector<float> perColumnCount(alnLength, 0.0);
 
-    std::vector<bool> ids(numSequences);  
-    std::fill(ids.end() - 2, ids.end(), true);
-    double sum = 0.0;
-    int numPairs = 0;
+    float sum = 0.0;
+    int numPairs = numSequences * (numSequences - 1) / 2;
 
+#pragma omp parallel reduction(+:sum) reduction(vsum:perColumnScore,perColumnCount)
+{
     LDDTCalculator *lddtcalculator = new LDDTCalculator(maxSeqLength + 1, maxSeqLength + 1);
-    int prevqId = -1;
-    do {
-        numPairs++;
-        
-        // Get the qId/tId of this permutation
-        int qId = -1;
-        int tId = -1;
-        for (int i = 0; i < numSequences; ++i) {
-            if (ids[i] && qId == -1) { qId = i; continue; }
-            if (ids[i] && tId == -1) { tId = i; break; }
-        }
-        
-        // lengths
-        // per column lddt idx <-> msa idx
-        std::vector<int> match_to_msa;
-        
-        // Generate a CIGAR string from qId-tId sub-alignment, ignoring -- columns
-        // e.g. --X-XX-X---XX-
-        //      Y---YYYY---YYY
-        //          MMDM   MM
-        Matcher::result_t res;
-        res.backtrace = "";
-        bool started = false;  // flag for first Match column in backtrace
-        int m  = 0;  // index in msa
-        int qr = 0;  // index in seq
-        int tr = 0;
-        while (m < alnLength) {
-            char qc = sequences[qId][m];
-            char tc = sequences[tId][m];
-            if (qc == '-' && tc == '-') {
-                // Skip gap columns
-            } else if (qc == '-') {
-                if (started) res.backtrace.push_back('D');
-                ++tr;
-            } else if (tc == '-') {
-                if (started) res.backtrace.push_back('I');
-                ++qr;
-            } else {
-                if (!started) {
-                    started = true;  
-                    res.qStartPos = qr;
-                    res.dbStartPos = tr;
-                }
-                match_to_msa.push_back(m);
-                res.backtrace.push_back('M');
-                res.qEndPos = qr;
-                res.dbEndPos = tr;
-                ++qr;
-                ++tr;
-            }
-            ++m;
-        }
-       
-        // If no alignment between the two sequences, skip
-        if (res.backtrace.length() == 0)
-            continue;
-        
-        // Remove D/I from backtrace after last M
-        res.backtrace.shrink_to_fit();
-        size_t i;
-        for (i = res.backtrace.length() - 1; res.backtrace[i] != 'M'; i--);
-        res.backtrace.erase(i + 1, res.backtrace.length() - i);
-        
-        // Get c-alpha info from _ca db for q and t
+    std::vector<Matcher::result_t> results(numPairs);
+    
+    for (int i = 0; i < numSequences; i++) {
         Coordinate16 qcoords;
-        char *qcadata = seqDbrCA.getData(indices[qId], 0);
-        size_t qCaLength = seqDbrCA.getEntryLen(indices[qId]);
-        float *queryCaData = qcoords.read(qcadata, qr, qCaLength);
+        char *qcadata = seqDbrCA.getData(indices[i], 0);
+        size_t qCaLength = seqDbrCA.getEntryLen(indices[i]);
+        float *queryCaData = qcoords.read(qcadata, lengths[i], qCaLength);
         
-        Coordinate16 tcoords;
-        char *tcadata = seqDbrCA.getData(indices[tId], 0);
-        size_t tCaLength = seqDbrCA.getEntryLen(indices[tId]);
-        float *targetCaData = tcoords.read(tcadata, tr, tCaLength);
+        lddtcalculator->initQuery(lengths[i], queryCaData, &queryCaData[lengths[i]], &queryCaData[2 * lengths[i]]);
 
-        // Calculate LDDT using created result_t object
-        // Only re-initialise when query actually changes
-        if (prevqId != qId)
-            lddtcalculator->initQuery(qr, queryCaData, &queryCaData[qr], &queryCaData[2 * qr]);
-        prevqId = qId;
+#pragma omp for schedule(dynamic, 1)
+        for (int j = i + 1; j < numSequences; j++) {
+            Coordinate16 tcoords;
+            char *tcadata = seqDbrCA.getData(indices[j], 0);
+            size_t tCaLength = seqDbrCA.getEntryLen(indices[j]);
+            float *targetCaData = tcoords.read(tcadata, lengths[j], tCaLength);
+            
+            // lengths per column lddt idx <-> msa idx
+            std::vector<int> match_to_msa;
 
-        LDDTCalculator::LDDTScoreResult lddtres = lddtcalculator->computeLDDTScore(
-            tr,
-            res.qStartPos,
-            res.dbStartPos,
-            res.backtrace,
-            targetCaData,
-            &targetCaData[tr],
-            &targetCaData[2 * tr]
-        );
-        
-        for (int i = 0; i < lddtres.scoreLength; i++) {
-            if (lddtres.perCaLddtScore[i] == 0.0) continue;
-            size_t idx = match_to_msa[i];
-            perColumnSOP[idx] += (sequences[qId][idx] == sequences[tId][idx]);
-            perColumnScore[idx] += lddtres.perCaLddtScore[i];
-            perColumnCount[idx]++;
+            // Generate a CIGAR string from qId-tId sub-alignment, ignoring -- columns
+            // e.g. --X-XX-X---XX-
+            //      Y---YYYY---YYY
+            //          MMDM   MM
+            Matcher::result_t res;
+            res.backtrace = "";
+            bool started = false;  // flag for first Match column in backtrace
+            int m  = 0;  // index in msa
+            int qr = 0;  // index in seq
+            int tr = 0;
+            while (m < alnLength) {
+                char qc = sequences[i][m];
+                char tc = sequences[j][m];
+                if (qc == '-' && tc == '-') {
+                    // Skip gap columns
+                } else if (qc == '-') {
+                    if (started) res.backtrace.push_back('D');
+                    ++tr;
+                } else if (tc == '-') {
+                    if (started) res.backtrace.push_back('I');
+                    ++qr;
+                } else {
+                    if (!started) {
+                        started = true;  
+                        res.qStartPos = qr;
+                        res.dbStartPos = tr;
+                    }
+                    match_to_msa.push_back(m);
+                    res.backtrace.push_back('M');
+                    res.qEndPos = qr;
+                    res.dbEndPos = tr;
+                    ++qr;
+                    ++tr;
+                }
+                ++m;
+            }
+
+            // Remove D/I from backtrace after last M
+            res.backtrace.shrink_to_fit();
+            size_t k;
+            for (k = res.backtrace.length() - 1; res.backtrace[k] != 'M'; k--);
+            res.backtrace.erase(k + 1, res.backtrace.length() - k);
+
+            LDDTCalculator::LDDTScoreResult lddtres = lddtcalculator->computeLDDTScore(
+                lengths[j],
+                res.qStartPos,
+                res.dbStartPos,
+                res.backtrace,
+                targetCaData,
+                &targetCaData[lengths[j]],
+                &targetCaData[2 * lengths[j]]
+            );
+            
+            for (int k = 0; k < lddtres.scoreLength; k++) {
+                if (lddtres.perCaLddtScore[k] == 0.0) continue;
+                int idx = match_to_msa[k];
+                perColumnScore[idx] += lddtres.perCaLddtScore[k];
+                perColumnCount[idx] += 1;
+            }
+            sum += lddtres.avgLddtScore;
         }
-       
-        sum += lddtres.avgLddtScore;
-    } while (std::next_permutation(ids.begin(), ids.end()));
+    }
     delete lddtcalculator;
+}
    
     std::cout << "Average MSA LDDT: " << sum / (double)numPairs << std::endl;
     
@@ -232,8 +225,6 @@ int msa2lddt(int argc, const char **argv, const Command& command) {
             entry.append(std::to_string(i));
             entry.append(",");
             entry.append(std::to_string(perColumnScore[i] / (double)perColumnCount[i]));
-            entry.append(",");
-            entry.append(std::to_string(perColumnSOP[i] / (double)perColumnCount[i]));
             entry.append("],");
             resultWriter.writeData(entry.c_str(), entry.length(), 0, 0, false, false);
         }
