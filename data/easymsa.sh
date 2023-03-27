db16dd099f6055a928ae09fbd533afdff2c71aa3
Cameron Gilchrist <clmgilchrist@gmail.com>
2023-03-27 17:31:36 +0900

    remove fake prefilter from easymsa workflow

--- data/easymsa.sh
+++ data/easymsa.sh
@@ -8,23 +8,6 @@ notExists() {
 	[ ! -f "$1" ]
 }
 
-# Generate fake prefilter file for all vs all alignments
-fake_pref() {
-    QDB="$1"
-    TDB="$2"
-    RES="$3"
-    # create link to data file which contains a list of all targets that should be aligned
-    # ln -s "${TDB}.index" "${RES}"
-	# TODO structurealign complains about no data file being found when using symlink
-	cp "${TDB}.index" "${RES}"
-    # create new index repeatedly pointing to same entry
-    # INDEX_SIZE="$(echo $(wc -c < "${TDB}.index"))"
-    INDEX_SIZE=$(wc -c < "${TDB}.index")
-    awk -v size="${INDEX_SIZE}" '{ print $1"\t0\t"size; }' "${QDB}.index" > "${RES}.index"
-    # create dbtype (7)
-    awk 'BEGIN { printf("%c%c%c%c",7,0,0,0); exit; }' > "${RES}.dbtype"
-}
-
 STRUCTUREDB="${TMP_PATH}/structures"
 if notExists "${STRUCTUREDB}.dbtype"; then
     # shellcheck disable=SC2086
