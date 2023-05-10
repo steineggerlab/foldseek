8eb36a26c6e009774ef83842dfd03305ca1a0b82
Milot Mirdita <milot@mirdita.de>
2023-05-11 00:29:59 +0900

    Update pdb database creation script for cluster searches

--- util/update_webserver_pdb/config.sh
+++ util/update_webserver_pdb/config.sh
@@ -6,10 +6,11 @@ CIF_SUBDIR="::ftp_data/structures/divided/mmCIF/" # subdirectory
 foldseek="./foldseek/bin/foldseek"
 
 # directories
+SCRATCH="/scratch_dir_that_will_be_deleted_rmrf_after_completion"
 MIRRORDIR="/parent_directory_to_download_all_pdb_files"
-PAST_DB_FOLDER="/directory_to_store_previously_generated_foldseek_databases"
-SYMLINK_FOLDER="/directory_for_symlinks_to_pdb_files"
-# log file
+
+# pdb download log file
 LOGFILE="file_to_record_logs_for_downloading_all_pdb_files"
 
+# cloud storage upload endpoint
 cloudflare_endpoint_url="endpoint_url_for_r2_cloudflarestorage_with_bucket_name=foldseek"
