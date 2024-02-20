5ac175fd50c604121795b1ffc07780a020bffe4e
sooyoung <ellen2g77@gmail.com>
2024-02-20 17:44:32 +0900

    erased default -c 0.8

--- src/strucclustutils/filtercomplex.cpp
+++ src/strucclustutils/filtercomplex.cpp
@@ -60,14 +60,17 @@ std::vector<unsigned int> selecHighestCoverage( std::map<unsigned int , std::map
 int filtercomplex(int argc, const char **argv, const Command &command) {
     LocalParameters &par = LocalParameters::getLocalInstance();
     par.parseParameters(argc, argv, command, true, 0, 0);
+    /*
     if  (par.covThr == false){
         par.covThr = 0.8;
     }
+    */
     const bool sameDB = par.db1.compare(par.db2) == 0 ? true : false;
     const bool touch = (par.preloadMode != Parameters::PRELOAD_MODE_MMAP);
     int dbaccessMode = (DBReader<unsigned int>::USE_INDEX);
     std::map<unsigned int, unsigned int> qKeyToSet;
     std::map<unsigned int, unsigned int> tKeyToSet;
+    char buffer[32];
 
     IndexReader* qDbr;
     qDbr = new IndexReader(par.db1, par.threads,  IndexReader::SRC_SEQUENCES, (touch) ? (IndexReader::PRELOAD_INDEX | IndexReader::PRELOAD_DATA) : 0, dbaccessMode);
@@ -85,11 +88,12 @@ int filtercomplex(int argc, const char **argv, const Command &command) {
 #ifdef OPENMP
     //localThreads = std::max(std::min((size_t)par.threads, alnDbr.getSize()), (size_t)1);
 #endif
-    const bool shouldCompress = par.dbOut == true && par.compressed == true;
-    const int dbType = par.dbOut == true ? Parameters::DBTYPE_GENERIC_DB : Parameters::DBTYPE_OMIT_FILE;
-    DBWriter resultWriter(par.db4.c_str(), par.db4Index.c_str(), 1, shouldCompress, dbType);
+    const bool shouldCompress = (par.compressed == true);
+    const int db4Type = Parameters::DBTYPE_CLUSTER_RES;
+    
+    DBWriter resultWriter(par.db4.c_str(), par.db4Index.c_str(), 1, shouldCompress, db4Type);
     resultWriter.open();
-    const bool isDb = par.dbOut;
+
     std::string qLookupFile = par.db1 + ".lookup";
     std::string tLookupFile = par.db2 + ".lookup";
     TranslateNucl translateNucl(static_cast<TranslateNucl::GenCode>(par.translationTable));
@@ -132,6 +136,7 @@ int filtercomplex(int argc, const char **argv, const Command &command) {
             unsigned int reslen = getTargetResidueLength(qDbr, qChainKeys);
             qComplexLength[qComplexId] = reslen;
         }
+        
         for (size_t queryComplexIdx = 0; queryComplexIdx < qComplexIdVec.size(); queryComplexIdx++) {
             std::map<unsigned int, unsigned int> qcovSum;
             std::map<unsigned int, unsigned int> tcovSum;
@@ -176,9 +181,7 @@ int filtercomplex(int argc, const char **argv, const Command &command) {
                 float dbcov = static_cast<float>(tcovSum[pair.first]) / static_cast<float>(tComplexLength[tChainKeyToComplexIdMap[assIdTodbKey[pair.first]]]);                
                 if (!checkFilterCriteria(qcov, dbcov, par.covMode, par.covThr)){
                     keysToDelete.push_back(pair.first);
-                }
-                
-               
+                }         
             }
             for (const auto& key : keysToDelete) {
                 qcovSum.erase(key);
@@ -210,19 +213,17 @@ int filtercomplex(int argc, const char **argv, const Command &command) {
                     selectedAssIDs = selecHighestCoverage(qcompIdToassIdToalnSum);
                     break;
             }
-
             for (const auto& pair : qcovSum){
                 if (std::find(selectedAssIDs.begin(), selectedAssIDs.end(), pair.first) != selectedAssIDs.end()){
-                    result += std::to_string(qComplexId)+ "\t"  + std::to_string(tChainKeyToComplexIdMap[assIdTodbKey[pair.first]])+ "\n" ;
+                    char *outpos = Itoa::u32toa_sse2(tChainKeyToComplexIdMap[assIdTodbKey[pair.first]], buffer);
+                    result.append(buffer, (outpos - buffer - 1));
+                    result.push_back('\n');
                 }
             }
-            resultWriter.writeData(result.c_str(), result.length(), qComplexId, 0, isDb, isDb);
+            resultWriter.writeData(result.c_str(), result.length(), qComplexId);
         }
     }
     resultWriter.close(true);
-    if (isDb == false) {
-        FileUtil::remove(par.db4Index.c_str());
-    }
     alnDbr.close();
     delete qDbr;
     if (sameDB == false) {
