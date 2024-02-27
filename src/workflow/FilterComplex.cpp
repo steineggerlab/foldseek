03c635e54d894ed980fc318dc5bdc5d1bfe06237
rachelse <seamustard52@gmail.com>
2024-02-27 14:14:40 +0900

    Changed ComplexCluster into FilterComplex

---
+++ src/workflow/FilterComplex.cpp
@@ -0,0 +1,14 @@
+#include <cassert>
+
+#include "FileUtil.h"
+#include "CommandCaller.h"
+#include "Util.h"
+#include "Debug.h"
+#include "LocalParameters.h"
+
+#include "filtercluster.sh.h"
+
+int complexcluster(int argc, const char **argv, const Command &command)
+{
+    return 0;
+}
