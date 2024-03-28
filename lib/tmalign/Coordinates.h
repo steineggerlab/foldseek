e44034e6c05b9ee40faf0cceea378b27a95b17c6
rachelse <seamustard52@gmail.com>
2024-03-28 14:16:42 +0900

    Implemented realloc function in Coordinate.h

--- lib/tmalign/Coordinates.h
+++ lib/tmalign/Coordinates.h
@@ -7,7 +7,7 @@
 #include "simd.h"
 
 struct Coordinates{
-    Coordinates(int size){
+    Coordinates(int size) : size(size) {
         x =(float*) mem_align(ALIGN_FLOAT, (size+VECSIZE_FLOAT)*sizeof(float));
         y =(float*) mem_align(ALIGN_FLOAT, (size+VECSIZE_FLOAT)*sizeof(float));
         z =(float*) mem_align(ALIGN_FLOAT, (size+VECSIZE_FLOAT)*sizeof(float));
@@ -27,5 +27,35 @@ struct Coordinates{
     float * x;
     float * y;
     float * z;
+    int size;
+
+    void realloc(int newsize){
+        if (allocated == false) {
+            x =(float*) mem_align(ALIGN_FLOAT, (size+VECSIZE_FLOAT)*sizeof(float));
+            y =(float*) mem_align(ALIGN_FLOAT, (size+VECSIZE_FLOAT)*sizeof(float));
+            z =(float*) mem_align(ALIGN_FLOAT, (size+VECSIZE_FLOAT)*sizeof(float));
+            allocated = true;
+            size = newsize;
+        } else {
+            if (newsize > size) {
+                float* new_x =(float*) mem_align(ALIGN_FLOAT, (newsize+VECSIZE_FLOAT)*sizeof(float));
+                float* new_y =(float*) mem_align(ALIGN_FLOAT, (newsize+VECSIZE_FLOAT)*sizeof(float));
+                float* new_z =(float*) mem_align(ALIGN_FLOAT, (newsize+VECSIZE_FLOAT)*sizeof(float));
+
+                if (x) memcpy(new_x, x, size * sizeof(float));
+                if (y) memcpy(new_y, y, size * sizeof(float));
+                if (z) memcpy(new_z, z, size * sizeof(float));
+
+                free(x);
+                free(y);
+                free(z);
+
+                x = new_x;
+                y = new_y;
+                z = new_z;
+                size = newsize;
+            }
+        }
+    }
 };
 #endif //STRUCCLUST_COORDINATES_H
