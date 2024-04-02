54865b51c879fce4edb9896046d6b07ecc833429
Woosub-Kim <woosubgo@gmail.com>
2024-04-02 14:57:10 +0900

    foldseek multimer tmalign update re

--- data/complexsearch.sh
+++ data/complexsearch.sh
@@ -22,18 +22,17 @@ if [ "$PREFMODE" != "EXHAUSTIVE" ]; then
             || fail "Expandcomplex died"
     fi
     if notExists "${TMP_PATH}/result_expand_aligned.dbtype"; then
-
-        if [ "$COMPLEX_ALIGNMENT_ALGO" == "tmalign" ]; then
-                 # shellcheck disable=SC2086
-                 "$MMSEQS" structurealign "${QUERYDB}" "${TARGETDB}" "${TMP_PATH}/result_expand_pref" "${TMP_PATH}/result_expand_aligned_tmp" -e 100 \
-                     || fail $COMPLEX_ALIGNMENT_ALGO "died"
-                 # shellcheck disable=SC2086
-                 "$MMSEQS" tmalign "${QUERYDB}" "${TARGETDB}" "${TMP_PATH}/result_expand_aligned_tmp" "${TMP_PATH}/result_expand_aligned" ${COMPLEX_ALIGN_PAR} \
-                     || fail $COMPLEX_ALIGNMENT_ALGO "died"
+        if [ "$COMPLEX_ALIGNMENT_ALGO" = "tmalign" ]; then
+            # shellcheck disable=SC2086
+            "$MMSEQS" structurealign "${QUERYDB}" "${TARGETDB}" "${TMP_PATH}/result_expand_pref" "${TMP_PATH}/result_expand_aligned_tmp" -e 100 \
+                || fail $COMPLEX_ALIGNMENT_ALGO "died"
+            # shellcheck disable=SC2086
+            "$MMSEQS" tmalign "${QUERYDB}" "${TARGETDB}" "${TMP_PATH}/result_expand_aligned_tmp" "${TMP_PATH}/result_expand_aligned" ${COMPLEX_ALIGN_PAR} \
+                || fail $COMPLEX_ALIGNMENT_ALGO "died"
         else
-        # shellcheck disable=SC2086
-        "$MMSEQS" $COMPLEX_ALIGNMENT_ALGO "${QUERYDB}" "${TARGETDB}" "${TMP_PATH}/result_expand_pref" "${TMP_PATH}/result_expand_aligned" ${COMPLEX_ALIGN_PAR} \
-            || fail $COMPLEX_ALIGNMENT_ALGO "died"
+            # shellcheck disable=SC2086
+            "$MMSEQS" $COMPLEX_ALIGNMENT_ALGO "${QUERYDB}" "${TARGETDB}" "${TMP_PATH}/result_expand_pref" "${TMP_PATH}/result_expand_aligned" ${COMPLEX_ALIGN_PAR} \
+                || fail $COMPLEX_ALIGNMENT_ALGO "died"
         fi
     fi
     RESULT="${TMP_PATH}/result_expand_aligned"
