2a5509b13079a5752f0d1862170b54433d8ac4b2
lasseReifenrath <reifenrathlasse@gmail.com>
2024-11-22 14:19:33 +0100

    created LoLAlign

---
+++ src/strucclustutils/LoLAlign.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <limits>
+#include <cfloat>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include "LoLAlign.h"
+
+
+LoLAlign::LoLAlign(int size) {
+    std::cout << "LoLAligner constructor" << std::endl;
+    return;
+}
+
+LoLAlign::~LoLAlign() {
+    std::cout << "LoLAligner destructor" << std::endl;
+    return;
+}
