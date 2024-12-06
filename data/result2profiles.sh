eadd19a8c90706f9bd6e801ad18237420c6be50f
SooyoungCha <ellen2g77@gmail.com>
2024-12-06 22:44:11 +0900

    result2profile in one module, supporting aa and 3di

---
+++ data/result2profiles.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+IN1="$1"
+IN2="$2"
+RESULT="$3"
+OUT="$4"
+
+if [ ! -d "${TMP_PATH}" ]; then
+    mkdir -p "${TMP_PATH}"
+fi
+
+# shellcheck disable=SC2086
+"$MMSEQS" base:result2profile "${IN1}" "${IN2}" "${RESULT}" "${OUT}" ${RESULT2PROFILES_PAR} \
+    || fail "result2profile died"
+
+# shellcheck disable=SC2086
+"$MMSEQS" base:result2profile "${IN1}_ss" "${IN2}_ss" "${RESULT}" "${OUT}_ss" ${RESULT2PROFILES_PAR} \
+    || fail "result2profile died"
+
+rm -f "${TMP_PATH}/result2profiles.sh"
