d9d681042a50651d92fcb3897ce4f48040207222
SooyoungCha <ellen2g77@gmail.com>
2024-12-30 19:37:38 +0900

    minor

--- data/makepaddeddb.sh
+++ data/makepaddeddb.sh
@@ -61,7 +61,6 @@ if [ "${CLUSEARCH_PAR}" = 0 ]; then
 else
     if [ -e "${IN}_ss.dbtype" ]; then
         # shellcheck disable=SC2086
-        # shellcheck disable=SC2086
         "$MMSEQS" lndb "${IN}_h"  "${OUT}_tmp_ss_h" ${VERBOSITY} \
             || fail "lndb died"
 
