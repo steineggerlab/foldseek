5c6664a34bd6208f5016c12610248aa55ace5d09
SooyoungCha <ellen2g77@gmail.com>
2024-12-30 19:20:25 +0900

    no tmp dir for makepaddedseqdb

--- data/makepaddeddb.sh
+++ data/makepaddeddb.sh
@@ -3,14 +3,6 @@
 IN="$1"
 OUT="$2"
 
-outname=$(basename "${OUT}")
-gpu_mapping1="${outname}_ss.gpu_mapping1"
-gpu_mapping2="${outname}_ss.gpu_mapping2"
-
-if [ ! -d "${TMP_PATH}" ]; then
-    mkdir -p "${TMP_PATH}"
-fi
-
 exists() {
 	[ -f "$1" ]
 }
@@ -18,28 +10,41 @@ exists() {
 if [ "${CLUSEARCH_PAR}" = 0 ]; then
     if [ -e "${IN}_ss.dbtype" ]; then
         # shellcheck disable=SC2086
-        "$MMSEQS" lndb "${IN}_h"  "${IN}_ss_h" ${VERBOSITY} \
+        "$MMSEQS" lndb "${IN}_h"  "${OUT}_tmp_ss_h" ${VERBOSITY} \
             || fail "lndb died"
 
         # shellcheck disable=SC2086
-        "$MMSEQS" base:makepaddedseqdb "${IN}_ss" "${OUT}_ss" ${MAKEPADDEDSEQDB_PAR} \
+        "$MMSEQS" lndb "${IN}_ss"  "${OUT}_tmp_ss" ${VERBOSITY} \
+            || fail "lndb died"
+
+        # shellcheck disable=SC2086
+        "$MMSEQS" base:makepaddedseqdb "${OUT}_tmp_ss" "${OUT}_ss" ${MAKEPADDEDSEQDB_PAR} \
             || fail "mmseqs makepaddedseqdb died"
 
-        awk '{ print $3"\t"$1; }' "${OUT}_ss.lookup" > "${TMP_PATH}/${gpu_mapping1}"
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${OUT}_tmp_ss" ${VERBOSITY} \
+            || fail "rmdb died"
+
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${OUT}_tmp_ss_h" ${VERBOSITY} \
+            || fail "rmdb died"
+
+        awk '{ print $3"\t"$1; }' "${OUT}_ss.lookup" > "${OUT}_ss.gpu_mapping1"
 
         if [ -e "${IN}.dbtype" ]; then
             # shellcheck disable=SC2086
-            "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping1}" "${IN}" "${OUT}" \
+            "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping1" "${IN}" "${OUT}" \
                 --subdb-mode 1 ${THREADS_PAR} \
                 || fail "renamedbkeys died"
         fi
 
         if [ -e "${IN}_ca.dbtype" ]; then
             # shellcheck disable=SC2086
-            "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping1}" "${IN}_ca" "${OUT}_ca" \
+            "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping1" "${IN}_ca" "${OUT}_ca" \
                 --subdb-mode 1 ${THREADS_PAR} \
                 || fail "renamedbkeys died"
         fi
+        rm -f -- "${OUT}_ss.gpu_mapping1"
     else
         if [ -e "${IN}.dbtype" ]; then
             # shellcheck disable=SC2086
@@ -56,68 +61,89 @@ if [ "${CLUSEARCH_PAR}" = 0 ]; then
 else
     if [ -e "${IN}_ss.dbtype" ]; then
         # shellcheck disable=SC2086
-        "$MMSEQS" lndb "${IN}_h" "${IN}_ss_h" ${VERBOSITY} \
+        # shellcheck disable=SC2086
+        "$MMSEQS" lndb "${IN}_h"  "${OUT}_tmp_ss_h" ${VERBOSITY} \
             || fail "lndb died"
 
         # shellcheck disable=SC2086
-        "$MMSEQS" base:makepaddedseqdb "${IN}_ss" "${OUT}_ss" ${MAKEPADDEDSEQDB_PAR} \
+        "$MMSEQS" lndb "${IN}_ss"  "${OUT}_tmp_ss" ${VERBOSITY} \
+            || fail "lndb died"
+
+        # shellcheck disable=SC2086
+        "$MMSEQS" base:makepaddedseqdb "${OUT}_tmp_ss" "${OUT}_ss" ${MAKEPADDEDSEQDB_PAR} \
             || fail "mmseqs makepaddedseqdb died"
 
-        awk '{ print $3"\t"$1; }' "${OUT}_ss.lookup" > "${TMP_PATH}/${gpu_mapping1}"
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${OUT}_tmp_ss" ${VERBOSITY} \
+            || fail "rmdb died"
+
+        # shellcheck disable=SC2086
+        "$MMSEQS" rmdb "${OUT}_tmp_ss_h" ${VERBOSITY} \
+            || fail "rmdb died"
+
+        awk '{ print $3"\t"$1; }' "${OUT}_ss.lookup" > "${OUT}_ss.gpu_mapping1"
 
         awk 'BEGIN{i=0} FNR==NR{name[$3]=1;print $3"\t"$1;i++; next} {if (!($1 in name)){print $1"\t"i; i++}}' \
-        "${OUT}_ss.lookup" "${IN}.lookup" > "${TMP_PATH}/${gpu_mapping2}"
+        "${OUT}_ss.lookup" "${IN}.lookup" > "${OUT}_ss.gpu_mapping2"
 
         # shellcheck disable=SC2086
-        "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping1}" "${IN}" "${OUT}" \
+        "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping1" "${IN}" "${OUT}" \
             --subdb-mode 1 ${THREADS_PAR} \
             || fail "renamedbkeys died"
 
         # shellcheck disable=SC2086
-        "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping1}" "${IN}_ca" "${OUT}_ca" \
+        "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping1" "${IN}_ca" "${OUT}_ca" \
             --subdb-mode 1 ${THREADS_PAR} \
             || fail "renamedbkeys died"
 
         # shellcheck disable=SC2086
-        "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping2}" "${IN}_seq" "${OUT}_seq" \
+        "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping2" "${IN}_seq" "${OUT}_seq" \
             --subdb-mode 1 ${THREADS_PAR} \
             || fail "renamedbkeys died"
 
         # shellcheck disable=SC2086
-        "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping2}" "${IN}_seq_ca" "${OUT}_seq_ca"  \
+        "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping2" "${IN}_seq_ca" "${OUT}_seq_ca"  \
             --subdb-mode 1 ${THREADS_PAR} \
             || fail "renamedbkeys died"
 
         # shellcheck disable=SC2086
-        "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping2}" "${IN}_seq_h" "${OUT}_seq_h"  \
+        "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping2" "${IN}_seq_h" "${OUT}_seq_h"  \
             --subdb-mode 1 ${THREADS_PAR} \
             || fail "renamedbkeys died"
         
         # shellcheck disable=SC2086
-        "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping2}" "${IN}_seq_ss" "${OUT}_seq_ss"  \
+        "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping2" "${IN}_seq_ss" "${OUT}_seq_ss"  \
             --subdb-mode 1 ${THREADS_PAR} \
             || fail "renamedbkeys died"
 
         if exists "${IN}_clu.dbtype"; then
             # shellcheck disable=SC2086
-            "$MMSEQS" filterdb "${IN}_clu" "${TMP_PATH}/${OUT}_clutmp" --mapping-file  "${TMP_PATH}/${gpu_mapping2}"  ${VERBOSITY} ${THREADS_PAR} \
+            "$MMSEQS" filterdb "${IN}_clu" "${OUT}_clutmp" --mapping-file  "${OUT}_ss.gpu_mapping2"  ${VERBOSITY} ${THREADS_PAR} \
                 || fail "filterdb died"
             # shellcheck disable=SC2086
-            "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping1}" "${TMP_PATH}/${OUT}_clutmp" "${OUT}_clu"  \
+            "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping1" "${OUT}_clutmp" "${OUT}_clu"  \
                 --subdb-mode 0 ${THREADS_PAR} \
                 || fail "renamedbkeys died"  
+
+            # shellcheck disable=SC2086
+            "$MMSEQS" rmdb "${OUT}_clutmp" ${VERBOSITY} \
+                || fail "rmdb died"
         fi
 
         if exists "${IN}_aln.dbtype"; then
             # shellcheck disable=SC2086
-            "$MMSEQS" filterdb "${IN}_aln" "${TMP_PATH}/${OUT}_alntmp" --mapping-file  "${TMP_PATH}/${gpu_mapping2}"  ${VERBOSITY} ${THREADS_PAR} \
+            "$MMSEQS" filterdb "${IN}_aln" "${OUT}_alntmp" --mapping-file  "${OUT}_ss.gpu_mapping2"  ${VERBOSITY} ${THREADS_PAR} \
                 || fail "filterdb died"
             # shellcheck disable=SC2086
-            "$MMSEQS" renamedbkeys "${TMP_PATH}/${gpu_mapping1}" "${TMP_PATH}/${OUT}_alntmp" "${OUT}_aln"  \
+            "$MMSEQS" renamedbkeys "${OUT}_ss.gpu_mapping1" "${OUT}_alntmp" "${OUT}_aln"  \
                 --subdb-mode 0 ${THREADS_PAR} \
                 || fail "renamedbkeys died"  
-
+            # shellcheck disable=SC2086
+            "$MMSEQS" rmdb "${OUT}_alntmp" ${VERBOSITY} \
+                || fail "rmdb died"
         fi
+        rm -f -- "${OUT}_ss.gpu_mapping1"
+        rm -f -- "${OUT}_ss.gpu_mapping2"
 
         rm "${OUT}_seq"
         rm "${OUT}_seq_ss"
@@ -133,16 +159,6 @@ else
     fi
 fi
 
-if [ -e "${IN}_ss.dbtype" ]; then
-    rm -f -- "${TMP_PATH}/${gpu_mapping1}"
-    rm -f -- "${TMP_PATH}/${gpu_mapping2}"
-    
-    if [ -e "${IN}_aln.dbtype" ]; then
-        "$MMSEQS" rmdb "${TMP_PATH}/${OUT}_alntmp" 
-    fi
-    if [ -e "${IN}_clu.dbtype" ]; then
-        "$MMSEQS" rmdb "${TMP_PATH}/${OUT}_clutmp" 
-    fi
+if [ -e "${OUT}.sh" ]; then
+  rm -f -- "${OUT}.sh"
 fi
-
-rm -f "${TMP_PATH}/makepaddeddb.sh"
--- src/strucclustutils/makepaddeddb.cpp
+++ src/strucclustutils/makepaddeddb.cpp
@@ -14,19 +14,10 @@ int makepaddeddb(int argc, const char **argv, const Command &command) {
     LocalParameters &par = LocalParameters::getLocalInstance();
     
     par.parseParameters(argc, argv, command, true, Parameters::PARSE_VARIADIC, 0);
-    // std::string program = par.db2 + ".sh";
-    std::string tmpDir = par.filenames.back(); 
-    std::string hash = SSTR(par.hashParameter(command.databases, par.filenames, *command.params));
-    if (par.reuseLatest) {
-        hash = FileUtil::getHashFromSymLink(tmpDir + "/latest");
-    }
-    tmpDir = FileUtil::createTemporaryDirectory(tmpDir, hash);
-    par.filenames.pop_back();
-    std::string program = tmpDir + "/makepaddeddb.sh";
+    std::string program = par.db2 + ".sh";
     FileUtil::writeFile(program, makepaddeddb_sh, makepaddeddb_sh_len);
     
     CommandCaller cmd;
-    cmd.addVariable("TMP_PATH", tmpDir.c_str());
     cmd.addVariable("THREADS_PAR", par.createParameterString(par.onlythreads).c_str());
     if (par.clusterSearch == 1) {
         cmd.addVariable("CLUSEARCH_PAR", "1");
