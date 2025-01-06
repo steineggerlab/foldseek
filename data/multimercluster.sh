c630f531071aeb83ec1ab8e8b85aa8d7a66fc351
SooyoungCha <ellen2g77@gmail.com>
2025-01-06 18:08:05 +0900

    change code in fake multimer db in multimercluster so that subdbs can be used too

--- data/multimercluster.sh
+++ data/multimercluster.sh
@@ -29,48 +29,111 @@ abspath() {
 # Shift initial DB to complexDB using soft-linking
 # $1: input db
 # $2: output db
+
 buildCmplDb() {
-    touch "${2}"
-    awk -F"\t" 'BEGIN {OFFSET=0}
-        FNR==NR{chain_len[$1]=$3;next}
-        {
-            if (!($3 in off_arr)) {
-                off_arr[$3]=OFFSET
+#     touch "${2}"
+#     awk -F"\t" 'BEGIN {OFFSET=0}
+#         FNR==NR{chain_len[$1]=$3;next}
+#         {
+#             if (!($3 in off_arr)) {
+#                 off_arr[$3]=OFFSET
+#             }
+#             cmpl_len[$3]+=chain_len[$1];OFFSET+=chain_len[$1]
+#         }
+#         END {
+#             for (cmpl in off_arr) {
+#                 print cmpl"\t"off_arr[cmpl]"\t"cmpl_len[cmpl]
+#             }
+#         }' "${1}.index" "${1}.lookup" > "${2}.index"
+#     ln -s "$(abspath "${1}")" "${2}.0"
+#     cp "${1}.dbtype" "${2}.dbtype"
+
+    awk -F"\t" 'FNR==NR{
+        split($2,parts,"_")
+        multname=parts[1]
+        for (j = 2; j < length(parts); j++) {
+            if (j < length(parts)){
+                multname=multname"_" 
             }
-            cmpl_len[$3]+=chain_len[$1];OFFSET+=chain_len[$1]
+            multname = multname parts[j]
         }
-        END {
-            for (cmpl in off_arr) {
-                print cmpl"\t"off_arr[cmpl]"\t"cmpl_len[cmpl]
+        multnameidx[multname]=$3; next
+    } {
+        split($1,words," ")
+        split(words[1],part,"_")
+        output_string=part[1]
+        for (j = 2; j < length(part); j++) {
+            if (j < length(part)){
+                output_string=output_string"_" 
             }
-        }' "${1}.index" "${1}.lookup" > "${2}.index"
-    ln -s "$(abspath "${1}")" "${2}.0"
-    cp "${1}.dbtype" "${2}.dbtype"
+            output_string = output_string part[j]
+        }
+        if (output_string != current_key) {
+            if (current_key != "") {
+                print multnameidx[current_key] "\t" concatenated;
+            }
+            current_key = output_string;
+            concatenated = $2;
+        } else {
+            concatenated = concatenated $2;
+        }
+    } END {
+        if (current_key != "") {
+            print multnameidx[current_key] "\t" concatenated;
+        }
+    }'  "${2}.lookup" "${1}" > "${3}"
 }
 
 buldCmplhDb(){
-    awk -F"\t" 'BEGIN {INDEXVAL=0}
-        {
-            split($2,words," ")
-            split(words[1],parts,"_")
-            output_string=parts[1]
-            for (j = 2; j < length(parts); j++) {
-                if (j < length(parts)){
-                    output_string=output_string"_" 
-                }
-                output_string = output_string parts[j]
-            }
-            headerstring=""
-            for (k = 2; k < length(words)+1; k++) {
-                headerstring = headerstring words[k]" "
-            }
-            if (!(output_string in gogo)){
-                print INDEXVAL"\t"output_string" "headerstring
-                INDEXVAL++
-            }
-            gogo[output_string]=1
+    # awk -F"\t" 'BEGIN {INDEXVAL=0}
+    #     {
+    #         split($2,words," ")
+    #         split(words[1],parts,"_")
+    #         output_string=parts[1]
+    #         for (j = 2; j < length(parts); j++) {
+    #             if (j < length(parts)){
+    #                 output_string=output_string"_" 
+    #             }
+    #             output_string = output_string parts[j]
+    #         }
+    #         headerstring=""
+    #         for (k = 2; k < length(words)+1; k++) {
+    #             headerstring = headerstring words[k]" "
+    #         }
+    #         if (!(output_string in gogo)){
+    #             print INDEXVAL"\t"output_string" "headerstring
+    #             INDEXVAL++
+    #         }
+    #         gogo[output_string]=1
             
-        }' "${1}" > "${2}"
+    #     }' "${1}" > "${2}"
+
+    awk -F"\t" 'FNR==NR{
+        split($2,parts,"_")
+        multname=parts[1]
+        for (j = 2; j < length(parts); j++) {
+            if (j < length(parts)){
+                multname=multname"_" 
+            }
+            multname = multname parts[j]
+        }
+        multnameidx[multname]=$3; next
+    }{
+        split($2,words," ")
+        split(words[1],part,"_")
+        output_string=part[1]
+        for (j = 2; j < length(part); j++) {
+            if (j < length(part)){
+                output_string=output_string"_" 
+            }
+            output_string = output_string part[j]
+        }
+        headerstring=""
+        for (k = 2; k < length(words)+1; k++) {
+            headerstring = headerstring words[k]" "
+        }
+        print multnameidx[output_string]"\t"output_string" "headerstring
+    }' "${2}.lookup" "${1}" | sort | uniq > "${3}"
 }
 
 
@@ -91,21 +154,41 @@ fi
 # shift query DB, .index, .dbtype
 if notExists "${TMP_PATH}/multimer_db.dbtype"; then    
     # build complex db as output
-    buildCmplDb "${INPUT}" "${TMP_PATH}/multimer_db"
+    # buildCmplDb "${INPUT}" "${TMP_PATH}/multimer_db"
+    # shellcheck disable=SC2086
+    "$MMSEQS" createtsv "${INPUT}" "${INPUT}" "${TMP_PATH}/chain_db.tsv" --threads 1 \
+        || fail "createtsv died"  
+    buildCmplDb "${TMP_PATH}/chain_db.tsv" "${INPUT}" "${TMP_PATH}/multimer.tsv"
+    # shellcheck disable=SC2086
+    "$MMSEQS" tsv2db "${TMP_PATH}/multimer.tsv" "${TMP_PATH}/multimer_db" --output-dbtype 0 ${VERBOSITY_PAR} \
+        || fail "tsv2db died"
 fi
 
+BASEIN=$(basename "${INPUT}")
 # Shift _h, _h.dbtype
 if notExists "${TMP_PATH}/multimer_db_h.dbtype"; then
-    # # shellcheck disable=SC2086
-    # "$MMSEQS" tsv2db "${INPUT}.source" "${TMP_PATH}/complex_db_header_tmp" ${VERBOSITY_PAR} \
-    #     || fail "tsv2db died"
+    cut -f1 "${INPUT}".index > "${TMP_PATH}/idxlist"
+
     # shellcheck disable=SC2086
-    # "$MMSEQS" createtsv "${INPUT}" "${INPUT}_h" "${TMP_PATH}/chain_db_h.tsv" ${VERBOSITY_PAR} \
-    "$MMSEQS" createtsv "${INPUT}" "${INPUT}_h" "${TMP_PATH}/chain_db_h.tsv" --threads 1 \
+    "$MMSEQS" lndb "${INPUT}" "${TMP_PATH}/${BASEIN}tmp"  ${VERBOSITY} \
+        || fail "lndb died"
+
+    # shellcheck disable=SC2086
+    "$MMSEQS" rmdb "${TMP_PATH}/${BASEIN}tmp_h" ${VERBOSITY_PAR}
+    
+    # shellcheck disable=SC2086
+    "$MMSEQS" base:createsubdb "${TMP_PATH}/idxlist" "${INPUT}_h" "${TMP_PATH}/${BASEIN}tmp_h" --subdb-mode 0 ${VERBOSITY_PAR} \
+        || fail "createsubdb died"
+    
+    # shellcheck disable=SC2086
+    "$MMSEQS" createtsv "${TMP_PATH}/${BASEIN}tmp" "${TMP_PATH}/${BASEIN}tmp_h" "${TMP_PATH}/chain_db_h.tsv" --threads 1 \
         || fail "createtsv died"
-    buldCmplhDb "${TMP_PATH}/chain_db_h.tsv" "${TMP_PATH}/multimer_header.tsv"
+
+    sort "${TMP_PATH}/chain_db_h.tsv" > "${TMP_PATH}/chain_db_h.tsvtmp"
+    mv "${TMP_PATH}/chain_db_h.tsvtmp" "${TMP_PATH}/chain_db_h.tsv"
+    buldCmplhDb "${TMP_PATH}/chain_db_h.tsv" "${INPUT}" "${TMP_PATH}/multimer_header.tsv"
     # shellcheck disable=SC2086
-    "$MMSEQS" tsv2db "${TMP_PATH}/multimer_header.tsv" "${TMP_PATH}/multimer_db_h" ${VERBOSITY_PAR} \
+    "$MMSEQS" tsv2db "${TMP_PATH}/multimer_header.tsv" "${TMP_PATH}/multimer_db_h" --output-dbtype 12 ${VERBOSITY_PAR} \
         || fail "tsv2db died"
 fi
 
@@ -118,7 +201,6 @@ if notExists "${RESULT}.dbtype"; then
     # shellcheck disable=SC2086
     "$MMSEQS" mvdb "${TMP_PATH}/multimer_filt_info" "${RESULT}_filt_info" \
         || fail "mv died"
-
 fi
 
 if [ -n "${REMOVE_TMP}" ]; then
@@ -126,7 +208,13 @@ if [ -n "${REMOVE_TMP}" ]; then
     "$MMSEQS" rmdb "${TMP_PATH}/multimer_filt" ${VERBOSITY_PAR}
     # shellcheck disable=SC2086
     "$MMSEQS" rmdb "${TMP_PATH}/multimer_result" ${VERBOSITY_PAR}
+    # shellcheck disable=SC2086
+    "$MMSEQS" rmdb "${TMP_PATH}/${INPUT}tmp_h" ${VERBOSITY_PAR}
+    # shellcheck disable=SC2086
+    "$MMSEQS" rmdb "${TMP_PATH}/${INPUT}tmp" ${VERBOSITY_PAR}
     rm "${TMP_PATH}/chain_db_h.tsv"
+    rm "${TMP_PATH}/chain_db.tsv"
+    rm "${TMP_PATH}/multimer.tsv"
     rm "${TMP_PATH}/multimer_header.tsv"
     rm -rf "${TMP_PATH}/multimersearch_tmp"
     rm -f "${TMP_PATH}/multimercluster.sh"
