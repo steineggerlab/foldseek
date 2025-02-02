7077479f1f8ede11171c6e2f1ab234d658ac183c
SooyoungCha <ellen2g77@gmail.com>
2025-02-02 22:01:23 +0900

    multimercluster supports subdb

--- data/multimercluster.sh
+++ data/multimercluster.sh
@@ -48,6 +48,32 @@ buildCmplDb() {
     cp "${1}.dbtype" "${2}.dbtype"
 }
 
+getLookup() {
+    awk 'FNR==NR{idx[$1]=1;next} {
+        if ($1 in idx) {
+            print $1"\t"$3
+        }
+    }' "${1}.index" "${1}.lookup" > "${2}.lookuptmp"
+}
+
+getSource() {
+    awk 'FNR==NR{idx[$2]=1;next} {
+        if ($1 in idx) {
+            print
+        }
+    }' "${2}.lookuptmp" "${1}.source" > "${2}.sourcetmp"
+}
+
+buildIndex() {
+    sort -nk2 "${1}.index" > "${1}.indextmp"
+    awk 'FNR==NR{chainMult[$1]=$2; next} {
+        print chainMult[$1]"\t"$2"\t"$3
+    }' "${1}.lookuptmp" "${1}.indextmp" > "${1}.indextmp2"
+    awk '!seen[$1]++ {
+         printf "%d\t%d\t", $1, $2; sum[$1] = $3; next 
+         } { sum[$1] += $3 } { print sum[$1] 
+         }' "${1}.indextmp2" > "${1}.index"
+}
 
 if notExists "${TMP_PATH}/multimer_result.dbtype"; then
     # shellcheck disable=SC2086
@@ -63,13 +89,14 @@ fi
 
 # shift query DB, .index, .dbtype
 if notExists "${TMP_PATH}/multimer_db.dbtype"; then
-    # build complex db as output
-    buildCmplDb "${INPUT}" "${TMP_PATH}/multimer_db"
-fi
-
-# Shift _h, _h.dbtype
-if notExists "${TMP_PATH}/multimer_db_h.dbtype"; then
+    getLookup "${INPUT}" "${TMP_PATH}/multimer_db"
+    getSource "${INPUT}" "${TMP_PATH}/multimer_db"
     # shellcheck disable=SC2086
+    "$MMSEQS" base:createsubdb "${TMP_PATH}/multimer_db.lookuptmp" "${INPUT}" "${TMP_PATH}/multimer_db"  --subdb-mode 0 ${VERBOSITY_PAR} \
+        || fail "createsubdb died"
+    buildIndex "${TMP_PATH}/multimer_db"
+    # shellcheck disable=SC2086
+    # "$MMSEQS" tsv2db "${TMP_PATH}/multimer_db.sourcetmp" "${TMP_PATH}/multimer_db_h" --output-dbtype 12 ${VERBOSITY_PAR} \
     "$MMSEQS" tsv2db "${INPUT}.source" "${TMP_PATH}/multimer_db_h" --output-dbtype 12 ${VERBOSITY_PAR} \
         || fail "tsv2db died"
 fi
