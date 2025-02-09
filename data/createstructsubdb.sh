69b74dcd3c2903aef5a0b2794eabefea5b15f8b8
SooyoungCha <ellen2g77@gmail.com>
2025-02-09 20:05:20 +0900

    createsubdb can get penguin as input

--- data/createstructsubdb.sh
+++ data/createstructsubdb.sh
@@ -8,13 +8,13 @@ fi
 
 if [ -e "${IN}_ss.dbtype" ]; then
     # shellcheck disable=SC2086
-    "$MMSEQS" base:createsubdb "${LIST}" "${IN}_ss" "${OUT}_ss" ${CREATESTRUCTSUBDB_PAR} \
+    "$MMSEQS" base:createsubdb "${OUT}.index" "${IN}_ss" "${OUT}_ss" ${CREATESTRUCTSUBDB_PAR} \
         || fail "createsubdb died"
 fi
 
 if [ -e "${IN}_ca.dbtype" ]; then
     # shellcheck disable=SC2086
-    "$MMSEQS" base:createsubdb "${LIST}" "${IN}_ca" "${OUT}_ca" ${CREATESTRUCTSUBDB_PAR} \
+    "$MMSEQS" base:createsubdb "${OUT}.index" "${IN}_ca" "${OUT}_ca" ${CREATESTRUCTSUBDB_PAR} \
         || fail "createsubdb died"
 fi
 
