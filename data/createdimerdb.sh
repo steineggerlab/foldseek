12dd8441e9801b45ab7a41c8952968c949acce6b
SooyoungCha <ellen2g77@gmail.com>
2025-02-12 15:16:06 +0900

    createdimerdb draft

--- data/createdimerdb.sh
+++ data/createdimerdb.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -e
 
-#getdimeridxlist as idxlist
+
+#currently checking if shraing monomers among dimers is possible. (So the lookup file's first column might not be unique)
+#it seems working. I'll update this code later
+
+
+# TODO: getmonmer index list as idxlist
 
 if [ -e "${IN}.dbtype" ]; then
     # shellcheck disable=SC2086
-    "$MMSEQS" base:createsubdb "idxlist" "${IN}" "${OUT}" ${CREATESTRUCTSUBDB_PAR} \
+    "$MMSEQS" createsubdb "idxlist" "${IN}" "${OUT}" ${CREATESTRUCTSUBDB_PAR} \
         || fail "createsubdb died"
+    rm -- "${OUT}_h" "${OUT}_h.dbtype" "${OUT}_h.index"
+    "$MMSEQS" base:createsubdb "idxlist" "${IN}_h" "${OUT}_h" ${CREATESTRUCTSUBDB_PAR} \
+        || fail "createsubdb died"
+    rm -- "${OUT}.lookup" "${OUT}.source" 
 fi
 
-if [ -e "${IN}_ss.dbtype" ]; then
-    # shellcheck disable=SC2086
-    "$MMSEQS" base:createsubdb "idxlist" "${IN}_ss" "${OUT}_ss" ${CREATESTRUCTSUBDB_PAR} \
-        || fail "createsubdb died"
-fi
-
-if [ -e "${IN}_ca.dbtype" ]; then
-    # shellcheck disable=SC2086
-    "$MMSEQS" base:createsubdb "idxlist"  "${IN}_ca" "${OUT}_ca" ${CREATESTRUCTSUBDB_PAR} \
-        || fail "createsubdb died"
-fi
+# TODO: make source file of all dimers, including "DI0", "DI1", "DI2".. 
+# TODO: make lookup file reflecting all monomer-dimer relation
