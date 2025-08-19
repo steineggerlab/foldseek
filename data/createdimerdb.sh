e4406e854fec9aef862a4ea52bc316bf3b7eda83
SooyoungCha <ellen2g77@gmail.com>
2025-08-19 15:07:53 +0900

    make createdimerdb faster, lookup shorter

--- data/createdimerdb.sh
+++ data/createdimerdb.sh
@@ -1,153 +1,53 @@
 #!/bin/sh -e
 
 
-#1. sharing monomers by changing lookup files to be possible to have redundant first column -> failed while expandmultimer & scoremultimer
-#2. sharing monomers by changing index files to share second&third columns -> it reduces db memory , but it doesn't reduce computation time
-
-# get list of each chain's id of multimers in chainidxlist (remove monomer chainids)
-# get dimer's original chainid and name in ${OUT}.lookuptmp
-
 exists() {
 	[ -f "$1" ]
 }
 
 if [ -e "${IN}.dbtype" ]; then
-    awk 'FNR==NR{
-        name[$1]=1; next
-    } {
-        if ($1 in name) {
-            print $0
-        }
-    }' "${IN}.index" "${IN}.lookup" > "${TMP_PATH}/sublookup"
-
-    awk -v firstout="${TMP_PATH}/lookuptmp" 'BEGIN {prev = -1; mon = 0; idx = -1; names = -1} {
-        if(!($3 == prev)) {
-            if (mon > 1) {
-                split(idx, words, " ")
-                split(names, namesplit, " ")
-                for (i = 1; i <= mon - 1; i++) {
-                    print words[i]
-                    for (j = i + 1; j <= mon; j++) {
-                        print words[i]"\t"namesplit[i] >> firstout
-                        print words[j]"\t"namesplit[j] >> firstout
-                    }
-                }
-                print words[mon]
-            }
-            prev = $3
-            mon = 1
-            idx = $1
-            names = $2
-        } else {
-            mon ++
-            idx = idx" "$1
-            names = names" "$2
-        }
-    } END {
-        if (mon > 1) {
-            split(idx, words, " ")
-            split(names, namesplit, " ")
-            for (i = 1; i <= mon - 1; i++) {
-                print words[i]
-                for (j = i + 1; j <= mon; j++) {
-                    print words[i]"\t"namesplit[i] >> firstout
-                    print words[j]"\t"namesplit[j] >> firstout
-                }
-            }
-            print words[mon]
-        }
-    }' "${TMP_PATH}/sublookup" > "${TMP_PATH}/chainidxlist"
-
-    awk -v secondout="${TMP_PATH}/reallookup" '{
-        print NR-1"\tDI"int((NR-1)/2)"_"$2"\t"int((NR-1)/2) >> secondout
-        print NR-1"\t"$1
-    }' "${TMP_PATH}/lookuptmp" > "${TMP_PATH}/realIdx_originalIdx"
-
-    awk '{print $3"\t"$2}' "${TMP_PATH}/reallookup" | awk -F'_' 'NR%2==1{OFS="_"; $NF=""; sub(/_$/, "", $0); print $0}' > "${TMP_PATH}/realsource" 
-
     # shellcheck disable=SC2086
-    "$MMSEQS" createsubdb "${TMP_PATH}/chainidxlist" "${IN}" "${TMP_PATH}/out" --subdb-mode 0 ${VERBOSITY_PAR} \
-        || fail "createsubdb died"
-
-    if exists "${IN}_id"; then
-        "$MMSEQS" lndb "${IN}_id" "${TMP_PATH}/out_id" \
-        || fail "lndb died"
-    fi
-
-    if exists "${TMP_PATH}/out.lookup" ; then
-        rm -- "${TMP_PATH}/out.lookup"
-    fi
-    if exists "${TMP_PATH}/out.source" ; then
-        rm -- "${TMP_PATH}/out.source"
-    fi
-    
-    # shellcheck disable=SC2086
-    "$MMSEQS" base:createsubdb "${TMP_PATH}/chainidxlist" "${IN}_h" "${TMP_PATH}/out_h" --subdb-mode 0 ${VERBOSITY_PAR} \
-        || fail "createsubdb died"
-    mv "${TMP_PATH}/reallookup" "${TMP_PATH}/dimerdb.lookup"
-    mv "${TMP_PATH}/realsource" "${TMP_PATH}/dimerdb.source"
-
-    awk 'FNR==NR{name[$1]=$2"\t"$3; next}{print $1"\t"name[$2]}' "${TMP_PATH}/out_h.index" "${TMP_PATH}/realIdx_originalIdx" > "${TMP_PATH}/dimerdb_h.index"
-    awk 'FNR==NR{name[$1]=$2"\t"$3; next}{print $1"\t"name[$2]}' "${TMP_PATH}/out.index" "${TMP_PATH}/realIdx_originalIdx" > "${TMP_PATH}/dimerdb.index"
-    awk 'FNR==NR{name[$1]=$2"\t"$3; next}{print $1"\t"name[$2]}' "${TMP_PATH}/out_ss.index" "${TMP_PATH}/realIdx_originalIdx" > "${TMP_PATH}/dimerdb_ss.index"
-    awk 'FNR==NR{name[$1]=$2"\t"$3; next}{print $1"\t"name[$2]}' "${TMP_PATH}/out_ca.index" "${TMP_PATH}/realIdx_originalIdx" > "${TMP_PATH}/dimerdb_ca.index"
-    if exists "${TMP_PATH}/out_id.index"; then
-        awk 'FNR==NR{name[$1]=$2"\t"$3; next}{print $1"\t"name[$2]}' "${TMP_PATH}/out_id.index" "${TMP_PATH}/realIdx_originalIdx" > "${TMP_PATH}/dimerdb_id.index"
-    fi
-    
-    mv "${TMP_PATH}/out_h" "${TMP_PATH}/dimerdb_h"
-    mv "${TMP_PATH}/out" "${TMP_PATH}/dimerdb"
-    mv "${TMP_PATH}/out_ss" "${TMP_PATH}/dimerdb_ss"
-    mv "${TMP_PATH}/out_ca" "${TMP_PATH}/dimerdb_ca"
-    mv "${TMP_PATH}/out_h.dbtype" "${TMP_PATH}/dimerdb_h.dbtype"
-    mv "${TMP_PATH}/out.dbtype" "${TMP_PATH}/dimerdb.dbtype"
-    mv "${TMP_PATH}/out_ss.dbtype" "${TMP_PATH}/dimerdb_ss.dbtype"
-    mv "${TMP_PATH}/out_ca.dbtype" "${TMP_PATH}/dimerdb_ca.dbtype"
-    if exists "${TMP_PATH}/out_id.index"; then
-        mv "${TMP_PATH}/out_id.dbtype" "${TMP_PATH}/dimerdb_id.dbtype"
-        mv "${TMP_PATH}/out_id" "${TMP_PATH}/dimerdb_id"
-    fi
-    # shellcheck disable=SC2086
-    "$MMSEQS" filterdimerdb "${TMP_PATH}/dimerdb" "${TMP_PATH}/contactlist" ${FILTERDIMERDB_PAR} \
+    "$MMSEQS" filterdimerdb "${IN}" "${TMP_PATH}/contactlist" ${FILTERDIMERDB_PAR} \
         || fail "filterdimerdb died"
+    sort -nk2 "${TMP_PATH}/contactlist.index" > "${TMP_PATH}/contactlist_2"
     # shellcheck disable=SC2086
-    "$MMSEQS" createsubdb "${TMP_PATH}/contactlist.index" "${TMP_PATH}/dimerdb" "${OUT}" --subdb-mode 0 ${VERBOSITY_PAR} \
+    "$MMSEQS" createsubdb "${TMP_PATH}/contactlist_2" "${IN}" "${OUT}" --subdb-mode 0 ${VERBOSITY_PAR} \
         || fail "createsubdb died"
     # shellcheck disable=SC2086
-    "$MMSEQS" mvdb "${TMP_PATH}/dimerdb_h" "${OUT}_h"
+    "$MMSEQS" rmdb "${OUT}_h"
+    # shellcheck disable=SC2086
+    "$MMSEQS" createsubdb "${TMP_PATH}/contactlist_2" "${IN}_h" "${OUT}_h" \
+        || fail "createsubdb died"
+    
+    sort -nk2 "${OUT}.index" | awk '{print NR"\t"$2"\t"$3}' > "${TMP_PATH}/${OUT}.index2"
+    sort -nk2 "${OUT}_ss.index" | awk '{print NR"\t"$2"\t"$3}' > "${TMP_PATH}/${OUT}_ss.index2"
+    sort -nk2 "${OUT}_ca.index" | awk '{print NR"\t"$2"\t"$3}' > "${TMP_PATH}/${OUT}_ca.index2"
+    sort -nk2 "${OUT}_h.index" | awk '{print NR"\t"$2"\t"$3}' > "${TMP_PATH}/${OUT}_h.index2"
+
+    mv "${TMP_PATH}/${OUT}.index2" "${OUT}.index"
+    mv "${TMP_PATH}/${OUT}_ss.index2" "${OUT}_ss.index"
+    mv "${TMP_PATH}/${OUT}_ca.index2" "${OUT}_ca.index"
+    mv "${TMP_PATH}/${OUT}_h.index2" "${OUT}_h.index"
+
+    rm "${OUT}.lookup" "${OUT}.source"
+    awk 'FNR==NR{name[$1]=$2; source[$1]=$3; next} BEGIN{num=0}{print num"\tDI"int(num/2)"_"name[$1]"\t"int(num/2)"\t"source[$1]; num++}' "${IN}.lookup" "${TMP_PATH}/contactlist_2" > "${TMP_PATH}/${OUT}.lookuptmp"
+    awk 'FNR==NR{name[$1]=$2; next} NR%2==1{print $3"\tDI"$3"_"name[$4]}' "${IN}.source" "${TMP_PATH}/${OUT}.lookuptmp" > "${OUT}.source"
+    cut -f1,2,3 "${TMP_PATH}/${OUT}.lookuptmp" > "${OUT}.lookup"
+    
     # shellcheck disable=SC2086
     "$MMSEQS" createtsv "${OUT}" "${OUT}_h" "${TMP_PATH}/tmpheader" --threads 1 ${VERBOSITY_PAR}
-    paste "${TMP_PATH}/dimerdb.lookup" "${TMP_PATH}/tmpheader" | awk -F"\t" '{print $1"\t"$2"\t"$5}' > "${TMP_PATH}/tmpheader2"
+    paste "${OUT}.lookup" "${TMP_PATH}/tmpheader" | awk -F"\t" '{print $1"\t"$2"\t"$5}' > "${TMP_PATH}/tmpheader2"
     # shellcheck disable=SC2086
     "$MMSEQS" rmdb "${OUT}_h"
     # shellcheck disable=SC2086
     "$MMSEQS" tsv2db "${TMP_PATH}/tmpheader2" "${OUT}_h" ${VERBOSITY_PAR}
-
-    mv "${TMP_PATH}/dimerdb.lookup" "${OUT}.lookup"
-    mv "${TMP_PATH}/dimerdb.source" "${OUT}.source"
-
 fi
 
 if [ -n "${REMOVE_TMP}" ]; then
-    rm "${TMP_PATH}/chainidxlist"
-    rm "${TMP_PATH}/lookuptmp"
-    rm "${TMP_PATH}/realIdx_originalIdx"
-    rm "${TMP_PATH}/out_ca.index"
-    rm "${TMP_PATH}/out_h.index"
-    rm "${TMP_PATH}/out.index"
-    rm "${TMP_PATH}/out_ss.index"
-    if [ -e "${IN}_id.index" ]; then
-        rm "${TMP_PATH}/out_id.index"
-    fi
-    # shellcheck disable=SC2086
-    "$MMSEQS" rmdb "${TMP_PATH}/dimerdb"
-    # shellcheck disable=SC2086
-    "$MMSEQS" rmdb "${TMP_PATH}/dimerdb_h"
-    # shellcheck disable=SC2086
-    "$MMSEQS" rmdb "${TMP_PATH}/dimerdb_ca"
-    # shellcheck disable=SC2086
-    "$MMSEQS" rmdb "${TMP_PATH}/dimerdb_ss"
     # shellcheck disable=SC2086
     "$MMSEQS" rmdb "${TMP_PATH}/contactlist"
-    rm -- "${TMP_PATH}/sublookup"
+    rm "${TMP_PATH}/contactlist_2"
+    rm "${TMP_PATH}/${OUT}.lookuptmp"
+    rm "${TMP_PATH}/tmpheader"
+    rm "${TMP_PATH}/tmpheader2"
 fi
--- src/strucclustutils/filterdimerdb.cpp
+++ src/strucclustutils/filterdimerdb.cpp
@@ -14,7 +14,6 @@
 #include <omp.h>
 #endif
 
-//one dimer db as an input, one dimer db with 
 int filterdimerdb(int argc, const char **argv, const Command &command) {
     LocalParameters &par = LocalParameters::getLocalInstance();
     par.parseParameters(argc, argv, command, true, 0, 0);
@@ -43,46 +42,44 @@ int filterdimerdb(int argc, const char **argv, const Command &command) {
     #ifdef OPENMP
         thread_idx = static_cast<unsigned int>(omp_get_thread_num());
     #endif
-        std::vector<int8_t> camol1, camol2;
-        std::vector<float> ca1, ca2;
         Coordinate16 qcoords;
         Coordinate16 tcoords;
 #pragma omp for schedule(static)
         for (size_t qCompIdx = 0; qCompIdx < qComplexIndices.size(); qCompIdx++) {
             unsigned int qComplexId = qComplexIndices[qCompIdx];
             std::vector<unsigned int> &qChainKeys = qComplexIdToChainKeysMap.at(qComplexId);
-            if (qChainKeys.size() != 2) {
+            if (qChainKeys.size() == 1) {
                 continue;
             }
-            unsigned int qChainIdx = 0;
-            unsigned int qChainKey = qChainKeys[qChainIdx];
-            unsigned int qChainDbId = qDbr.getId(qChainKey);
-            char *qcadata = qStructDbr.getData(qChainDbId, thread_idx);
-            size_t qCaLength = qStructDbr.getEntryLen(qChainDbId);
-            size_t qChainLen = qDbr.getSeqLen(qChainDbId);
-            float* qdata = qcoords.read(qcadata, qChainLen, qCaLength);
+            for (size_t qChainIdx = 0; qChainIdx < qChainKeys.size() - 1; qChainIdx++) {
+                for(size_t tChainIdx = qChainIdx+1; tChainIdx < qChainKeys.size(); tChainIdx++){
+                    unsigned int qChainKey = qChainKeys[qChainIdx];
+                    unsigned int qChainDbId = qDbr.getId(qChainKey);
+                    char *qcadata = qStructDbr.getData(qChainDbId, thread_idx);
+                    size_t qCaLength = qStructDbr.getEntryLen(qChainDbId);
+                    size_t qChainLen = qDbr.getSeqLen(qChainDbId);
+                    float* qdata = qcoords.read(qcadata, qChainLen, qCaLength);
 
-            unsigned int tChainIdx = 1;
-            unsigned int tChainKey = qChainKeys[tChainIdx];
-            unsigned int tChainDbId = qDbr.getId(tChainKey);
-            char *tcadata = qStructDbr.getData(tChainDbId, thread_idx);
-            size_t tCaLength = qStructDbr.getEntryLen(tChainDbId);
-            size_t tChainLen = qDbr.getSeqLen(tChainDbId);
-            float* tdata = tcoords.read(tcadata, tChainLen, tCaLength);
-            
-            float distanceThreshold = par.distanceThreshold;
-            std::vector<size_t> resIdx1, resIdx2;
-            const float squareThreshold = distanceThreshold * distanceThreshold;
-            findInterface(resIdx1, squareThreshold, qdata, tdata, qChainLen, tChainLen);
-            findInterface(resIdx2, squareThreshold, tdata, qdata, tChainLen, qChainLen);
-            if (resIdx1.size() >= 4 && resIdx2.size() >= 4) {  
-                resultWriter.writeData("0\n", 2, qChainKey, thread_idx);
-                resultWriter.writeData("0\n", 2, tChainKey, thread_idx);
+                    unsigned int tChainKey = qChainKeys[tChainIdx];
+                    unsigned int tChainDbId = qDbr.getId(tChainKey);
+                    char *tcadata = qStructDbr.getData(tChainDbId, thread_idx);
+                    size_t tCaLength = qStructDbr.getEntryLen(tChainDbId);
+                    size_t tChainLen = qDbr.getSeqLen(tChainDbId);
+                    float* tdata = tcoords.read(tcadata, tChainLen, tCaLength);
+
+                    float distanceThreshold = par.distanceThreshold;
+                    std::vector<size_t> resIdx1, resIdx2;
+                    const float squareThreshold = distanceThreshold * distanceThreshold;
+                    findInterface(resIdx1, squareThreshold, qdata, tdata, qChainLen, tChainLen);
+                    findInterface(resIdx2, squareThreshold, tdata, qdata, tChainLen, qChainLen);
+
+                    if (resIdx1.size() >= 4 && resIdx2.size() >= 4) {  
+                        resultWriter.writeData("0\n", 2, qChainKey, thread_idx);
+                        resultWriter.writeData("0\n", 2, tChainKey, thread_idx);
+                    }
+
+                }
             }
-            camol1.clear();
-            camol2.clear();
-            ca1.clear();
-            ca2.clear();
         }
     }
     resultWriter.close(true);
