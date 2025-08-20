3b7e0a60eb139d018df2229d50466be7a190dea4
SooyoungCha <ellen2g77@gmail.com>
2025-08-20 22:33:07 +0900

    MultimerUtil accepts subdb

--- src/strucclustutils/MultimerUtil.h
+++ src/strucclustutils/MultimerUtil.h
@@ -176,6 +176,7 @@ auto getChainId(ReaderType &dbr, unsigned int chainKey) -> decltype(dbr.sequence
 
 template <typename ReaderType>
 static void getKeyToIdMapIdToKeysMapIdVec(
+    DBReader<unsigned int> &alnDbr,
     ReaderType &dbr,
     const std::string &file,
     std::map<unsigned int, unsigned int> &chainKeyToComplexIdLookup,
@@ -189,7 +190,7 @@ static void getKeyToIdMapIdToKeysMapIdVec(
     char *data = (char *) lookupDB.getData();
     char *end = data + lookupDB.mappedSize();
     const char *entry[255];
-    std::vector<bool> isVistedSet(lookupDB.mappedSize(), false);
+    std::vector<bool> isVistedSet(alnDbr.getLastKey() + 1, false);
     while (data < end && *data != '\0') {
         const size_t columns = Util::getWordsOfLine(data, entry, 255);
         if (columns < 3) {
